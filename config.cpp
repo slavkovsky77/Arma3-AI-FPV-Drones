223f352e0786e14dc3a031988f0c3166b56902fd
root <martinslavkovsky7@gmail.com>
2024-12-15 23:44:29 +0100

    AT, AP module

--- config.cpp
+++ config.cpp
@@ -1,6 +1,10 @@
 class CfgPatches {
     class fpv_ai_drones {
-        units[] = {"FPV_AI_Drones_Module"};
+        units[] = {
+            "FPV_AI_Drones_Module",
+            "FPV_AI_Drones_AT_Module",
+            "FPV_AI_Drones_AP_Module"
+        };
         weapons[] = {};
         magazines[] = {};
         requiredVersion = 1.0;
@@ -111,6 +115,146 @@ class CfgVehicles {
 
         class ModuleDescription: ModuleDescription {
             description = "FPV AI Drones Module";
+            sync[] = {"All"};
+        };
+    };
+
+    class FPV_AI_Drones_AT_Module: FPV_AI_Drones_Module {
+        scope = 2;
+        displayName = "FPV AI Drones - Anti-Tank";
+        icon = "\A3\Modules_F_Curator\Data\iconSmoke_ca.paa";
+
+        class Attributes: AttributesBase {
+            class UnitKinds: Edit {
+                property = "FPV_AI_Drones_UnitKinds";
+                displayName = "Target Unit Types";
+                tooltip = "Array of unit types to target";
+                defaultValue = """LandVehicle,Car,Tank""";
+            };
+            class TargetSource: Combo {
+                property = "FPV_AI_Drones_TargetSource";
+                displayName = "Target Source";
+                tooltip = "Source of targets";
+                defaultValue = """vehicles""";
+                class Values {
+                    class AllUnits { name = "All Units"; value = "allUnits"; };
+                    class Vehicles { name = "Vehicles"; value = "vehicles"; };
+                };
+            };
+            class MaxDistance: Edit {
+                property = "FPV_AI_Drones_MaxDistance";
+                displayName = "Max Attack Distance";
+                tooltip = "Maximum distance for attack";
+                typeName = "NUMBER";
+                defaultValue = "7";
+            };
+            class MaxDistance2D: Edit {
+                property = "FPV_AI_Drones_MaxDistance2D";
+                displayName = "Max 2D Distance";
+                tooltip = "Maximum 2D distance";
+                typeName = "NUMBER";
+                defaultValue = "7";
+            };
+            class FinalHeight: Edit {
+                property = "FPV_AI_Drones_FinalHeight";
+                displayName = "Final Attack Height";
+                tooltip = "Final height for attack";
+                typeName = "NUMBER";
+                defaultValue = "5";
+            };
+            class AllowObjectParent: Checkbox {
+                property = "FPV_AI_Drones_AllowObjectParent";
+                displayName = "Allow Object Parent";
+                tooltip = "Allow targeting units in vehicles";
+                defaultValue = "true";
+            };
+            class CustomAmmo: Edit {
+                property = "FPV_AI_Drones_CustomAmmo";
+                displayName = "Custom Ammo Type";
+                tooltip = "Type of ammunition to use";
+                defaultValue = """SatchelCharge_Remote_Ammo""";
+            };
+            class MinTargetDistance: Edit {
+                property = "FPV_AI_Drones_MinTargetDistance";
+                displayName = "Min Target Distance";
+                tooltip = "Minimum distance to target";
+                typeName = "NUMBER";
+                defaultValue = "200";
+            };
+        };
+
+        class ModuleDescription: ModuleDescription {
+            description = "FPV AI Drones Module - Anti-Tank Variant";
+            sync[] = {"All"};
+        };
+    };
+
+    class FPV_AI_Drones_AP_Module: FPV_AI_Drones_Module {
+        scope = 2;
+        displayName = "FPV AI Drones - Anti-Personnel";
+        icon = "\A3\Modules_F_Curator\Data\iconSmoke_ca.paa";
+
+        class Attributes: AttributesBase {
+            class UnitKinds: Edit {
+                property = "FPV_AI_Drones_UnitKinds";
+                displayName = "Target Unit Types";
+                tooltip = "Array of unit types to target";
+                defaultValue = """Man""";
+            };
+            class TargetSource: Combo {
+                property = "FPV_AI_Drones_TargetSource";
+                displayName = "Target Source";
+                tooltip = "Source of targets";
+                defaultValue = """allUnits""";
+                class Values {
+                    class AllUnits { name = "All Units"; value = "allUnits"; };
+                    class Vehicles { name = "Vehicles"; value = "vehicles"; };
+                };
+            };
+            class MaxDistance: Edit {
+                property = "FPV_AI_Drones_MaxDistance";
+                displayName = "Max Attack Distance";
+                tooltip = "Maximum distance for attack";
+                typeName = "NUMBER";
+                defaultValue = "30";
+            };
+            class MaxDistance2D: Edit {
+                property = "FPV_AI_Drones_MaxDistance2D";
+                displayName = "Max 2D Distance";
+                tooltip = "Maximum 2D distance";
+                typeName = "NUMBER";
+                defaultValue = "8";
+            };
+            class FinalHeight: Edit {
+                property = "FPV_AI_Drones_FinalHeight";
+                displayName = "Final Attack Height";
+                tooltip = "Final height for attack";
+                typeName = "NUMBER";
+                defaultValue = "8";
+            };
+            class AllowObjectParent: Checkbox {
+                property = "FPV_AI_Drones_AllowObjectParent";
+                displayName = "Allow Object Parent";
+                tooltip = "Allow targeting units in vehicles";
+                defaultValue = "false";
+            };
+            class CustomAmmo: Edit {
+                property = "FPV_AI_Drones_CustomAmmo";
+                displayName = "Custom Ammo Type";
+                tooltip = "Type of ammunition to use";
+                defaultValue = """DemoCharge_Remote_Ammo""";
+            };
+            class MinTargetDistance: Edit {
+                property = "FPV_AI_Drones_MinTargetDistance";
+                displayName = "Min Target Distance";
+                tooltip = "Minimum distance to target";
+                typeName = "NUMBER";
+                defaultValue = "200";
+            };
+        };
+
+        class ModuleDescription: ModuleDescription {
+            description = "FPV AI Drones Module - Anti-Personnel Variant";
             sync[] = {"All"};
         };
     };
